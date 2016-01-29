0d260fc51a21e878756e2f5734b22699e71ea815
Aliyah Jessa <aliyah@aliyahmaliyah.com>
2016-01-29 18:50:55 +0000

    positive numbers using filter

--- answers.js
+++ answers.js
@@ -5,6 +5,15 @@ var myArray = [1,-1,2,-24,3,31];
 
 var findPos = myArray.forEach(function newArray(number){
     if (number > 0) {
-        console.log (number)
+        console.log (number);
 }});
 findPos;
+
+//write a function called getPositives that takes an array and uses its filter method 
+//to return a new array with only the positive numbers
+
+myArray.filter(function getPositives(number) {
+    if (number > 0) {
+        console.log (number);
+        }
+});
